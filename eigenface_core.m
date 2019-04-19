0db0525fd303d7208708c8b602bbb9cbb13152ba
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-04-19 10:10:02 +0300

    mid-step

--- eigenface_core.m
+++ eigenface_core.m
@@ -1,3 +1,27 @@
 function [m A eigenfaces pr_img] = eigenface_core (database_path)
-
+  
+  # 1
+  T = [];
+  for i = 1:10
+    this_path = char([database_path int2str(i) ".jpg"]);
+    this_image = double(rgb2gray(imread(this_path)));
+    column = this_image(:);
+    imshow(uint8(this_image));
+    disp([size(column, 1) size(column, 2)]);
+    T = [T column];
+    disp([size(T, 1) size(T, 2)]);
+  endfor
+  disp(T(1:10, 1:10));
+  # 2
+  m = mean(T);
+  disp(m);
+  # 3
+  A = T - m;
+  # 4
+  [lambda V] = 
+  EigFaces = A * V;
+  # end before full implementation, as to be able to test the task:
+  eigenfaces = 0;
+  pr_img = 0;
+  
 endfunction
--- task6.m
+++ task6.m
@@ -1,5 +1,5 @@
 function task6()
   path = "./task6/dataset/";
-  
+  [m A e p] = eigenface_core(path);
   
 end
