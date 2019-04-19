6d6b376f0e06e7fb3850d345cf47efb1f98166d2
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-04-19 06:25:51 +0300

    added all files to project, to facilitate testing

---
+++ face_recognition.m
@@ -0,0 +1,6 @@
+function [min_dist output_img_index] = 
+    face_recognition(image_path,m, A, eigenfaces, pr_img)
+  
+  # Ultima functie returneaza cea mai mica distanta si indicele proiectiei imaginii
+  # de la pasul 5 pentru care se obtinea cea mai mica distanta.
+endfunction
--- task6.m
+++ task6.m
@@ -1,22 +1,5 @@
-function task5()
+function task6()
   path = "./task6/dataset/";
-  # 1
-  T = [];
-  for i = 1:10
-    this_path = char([path int2str(i) ".jpg"]);
-    this_image = double(rgb2gray(imread(this_path)));
-    column = this_image(:);
-    #disp(size(column, 1));
-    #disp(size(column, 2));
-    #disp(column'(1, 1:5));
-    T = [T column];
-    #disp(T(1:5, size(T, 2))');
-  endfor
-  disp(T(1:10, 1:10));
-  # 2
-  m = mean(T);
-  disp(m);
-  # 3
-  A = T - m;
+  
   
 end
