413505de3b390c8393a57d9b64ac4458151b27ab
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-04-19 11:21:22 +0300

    done with this part, now back to Graphs

--- task6.m
+++ task6.m
@@ -2,5 +2,15 @@ function task6()
   path = "./task6/dataset/";
   [m A e p] = eigenface_core(path);
   image_path = "./task6/dataset/1.jpg";
-  face_recognition(image_path, m, A, e, p);
+  [dist index] = face_recognition(image_path, m, A, e, p);
+  
+  original = double(rgb2gray(imread(image_path)));
+  
+  start = (index - 1) * 40000;
+  stop = start + 40000 - 1;
+  image = reshape(A(start:stop), 200, 200);
+  
+  imshow(uint8(original));
+  #imshow(uint8(image));
+  
 end
